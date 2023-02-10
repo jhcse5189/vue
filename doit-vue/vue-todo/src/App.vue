c5725f99e058d2da23ab8566ae8c4ecd9e87d08d
bammer <jhcse5189@gmail.com>
2023-02-10 10:40:50 +0900

    book. doit-vue 6-6. animation

--- doit-vue/vue-todo/src/App.vue
+++ doit-vue/vue-todo/src/App.vue
@@ -76,5 +76,12 @@ export default {
   }
   input {
     border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
   }
 </style>
--- doit-vue/vue-todo/src/components/TodoList.vue
+++ doit-vue/vue-todo/src/components/TodoList.vue
@@ -1,14 +1,15 @@
 <template>
   <section>
-    <ul>
-      <li v-for="todoItem, index in propsdata" v-bind:key="todoItem">
+    <!-- vue animation -->
+    <transition-group name="list" tag="ul">
+      <li v-for="todoItem, index in propsdata" v-bind:key="todoItem" class="shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
         {{ todoItem }}
         <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
             <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </section>
 </template>
 
@@ -49,5 +50,12 @@ export default {
   .removeBtn {
     margin-left: auto;
     color: #de4343;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter-from, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
   }
 </style>
